4d5a46ae1643e316a8ceb261fa95aad4cd149f75
Bertrand Florat <bertrand@florat.net>
2020-03-10 09:27:59 +0100

    travail

---
+++ todolist-debut-ex2/src/test/java/com/acme/todolist/TodolistApplication.java
@@ -0,0 +1,26 @@
+package com.acme.todolist;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+/**
+ * Lanceur Spring boot
+ * 
+ * Note : comme cette classe n'est pas dans com.acme.todolist mais dans un sous package, il faut configurer finalement les 
+ * différents composants nécessaires
+ * @author bflorat
+ *
+ */
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.acme.todolist.rest_ui")
+public class TodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistApplication.class, args);
+	}
+
+}
